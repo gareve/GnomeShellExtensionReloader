85dced8652a539691a56617d21e87b16ed6c8679
Gabriel Rea Velasco <gareve25@gmail.com>
2022-01-14 23:36:28 +0000

    Remove hardcoded paths

--- extension.js
+++ extension.js
@@ -54,55 +54,53 @@ function runMyShell(cmd) {
 }
 
 function deleteAllVersionsOfExtension(uuid) {
-  const allUuids = Main.extensionManager.getUuids();
-
-  for (let i_uuid of allUuids) {
+  for (let i_uuid of Main.extensionManager.getUuids()) {
     if (!i_uuid.startsWith(uuid)) {
-      log(">>>> Ignoring extension removal: " + i_uuid);
       continue;
     }
-
     let extension = Main.extensionManager.lookup(i_uuid);
-    if (extension.type !== ExtensionType.PER_USER) {
+    if (!extension || extension.type !== ExtensionType.PER_USER) {
       continue;
     }
 
-    if (extension) {
-      Main.notify("Deleting Extension", i_uuid);
-      Main.extensionManager.unloadExtension(extension);
-      runMyShell(
-        "rm -rf /home/gareve/.local/share/gnome-shell/extensions/" + i_uuid
-      );
-    }
+    const extensionPath = GLib.build_filenamev([
+      global.userdatadir,
+      "extensions",
+      i_uuid,
+    ]);
+
+    Main.extensionManager.unloadExtension(extension);
+    runMyShell("rm -rf " + extensionPath);
   }
 }
 
 function installEphimeralExtension(uuid) {
   // based on https://stackoverflow.com/questions/62265594/gnome-shell-extension-install-possible-without-restart
-  let manager = Main.extensionManager;
+  const settings = getSettings();
+  const manager = Main.extensionManager;
   const nowTimestamp = Date.now();
   const ephUUID = uuid + "_eph_" + nowTimestamp;
-  const newDir = "/home/gareve/.local/share/gnome-shell/extensions/" + ephUUID;
-
-  log(">>>> Reinstall Extension " + ephUUID);
-
-  let dir = Gio.File.new_for_path(
-    GLib.build_filenamev([global.userdatadir, "extensions", ephUUID])
-  );
+  const ephExtensionPath = GLib.build_filenamev([
+    global.userdatadir,
+    "extensions",
+    ephUUID,
+  ]);
+  const ephExtensionDir = Gio.File.new_for_path(ephExtensionPath);
 
   const cmd =
-    "cp -r /home/gareve/Dropbox/Programacion/gnomeShell/" + uuid + " " + newDir;
+    "cp -r " +
+    Gio.File.new_for_path(settings.get_string("extension-path"))
+      .get_parent()
+      .get_path() +
+    " " +
+    ephExtensionPath;
   runMyShell(cmd);
 
   // Modifying metadata.json
   // TODO: Error handling
-  let metadataFile = dir.get_child("metadata.json");
-  let metadataContents, success_;
-
-  [success_, metadataContents] = metadataFile.load_contents(null);
-  metadataContents = ByteArray.toString(metadataContents);
-  let meta = JSON.parse(metadataContents);
-  log(metadataContents);
+  const metadataFile = ephExtensionDir.get_child("metadata.json");
+  const [success_, metadataContents] = metadataFile.load_contents(null);
+  const meta = JSON.parse(ByteArray.toString(metadataContents));
   meta.uuid = ephUUID;
   meta.name += " eph_" + nowTimestamp;
   let [success, tag] = metadataFile.replace_contents(
@@ -115,7 +113,7 @@ function installEphimeralExtension(uuid) {
 
   let extension = manager.createExtensionObject(
     ephUUID,
-    dir,
+    ephExtensionDir,
     ExtensionUtils.ExtensionType.PER_USER
   );
 
@@ -126,6 +124,10 @@ function installEphimeralExtension(uuid) {
       throw new Error(
         "Cannot add %s to enabled extensions gsettings key".format(ephUUID)
       );
+    Main.notify(
+      "Old Extension deleted & new Ephimeral Extension installed",
+      ephUUID
+    );
   } catch (e) {
     let extension = Main.extensionManager.lookup(ephUUID);
     if (extension) Main.extensionManager.unloadExtension(extension);
@@ -156,6 +158,22 @@ const MyPopup = GObject.registerClass(
 
       this.add_child(icon);
 
+      // Extension Label
+      let extensionMenuItem = new PopupMenu.PopupMenuItem(uuid, {
+        reactive: false,
+        activate: false,
+        hover: false,
+        can_focus: false,
+      });
+      settings.connect(`changed::extension-path`, () => {
+        extensionMenuItem.label.set_text(
+          Gio.File.new_for_path(settings.get_string("extension-path"))
+            .get_parent()
+            .get_basename()
+        );
+      });
+      this.menu.addMenuItem(extensionMenuItem);
+
       // Reload Extension
       let reload = new PopupMenu.PopupMenuItem("Reload Extension!");
       this.menu.addMenuItem(reload);
@@ -172,22 +190,6 @@ const MyPopup = GObject.registerClass(
       });
 
       this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-
-      // Extension Label
-      let extensionMenuItem = new PopupMenu.PopupMenuItem(uuid, {
-        reactive: false,
-        activate: false,
-        hover: false,
-        can_focus: false,
-      });
-      this.menu.addMenuItem(extensionMenuItem);
-      settings.connect(`changed::extension-path`, () => {
-        extensionMenuItem.label.set_text(
-          Gio.File.new_for_path(settings.get_string("extension-path"))
-            .get_parent()
-            .get_basename()
-        );
-      });
     }
   }
 );
